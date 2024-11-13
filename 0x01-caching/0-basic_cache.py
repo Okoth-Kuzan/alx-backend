b4bd3f828092bc3d99fd591abb4d513d41221497
Okoth-Kuzzan <terrance.e.okoth47@icloud.com>
2024-11-13 11:44:34 +0000

    0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" 0. Basic dictionary
+""" BaseCaching module
 """
 
-
-BaseCaching = __import__("base_caching").BaseCaching
-
-
-class BasicCache(BaseCaching):
-    """ BasicCache class. Inherits from BaseCaching.
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
     """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
 
     def put(self, key, item):
-        """ Must assign to the dictionary self.cache_data the item value for
-        the key key. If key or item is None, this method should not do anything
+        """ Add an item in the cache
         """
-        if key and item:
-            self.cache_data[key] = item
+        raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
-        """ Must return the value in self.cache_data linked to key. If key is
-        None or if the key doesn’t exist in self.cache_data, return None.
+        """ Get an item by key
         """
-        return self.cache_data.get(key, None)
+        raise NotImplementedError("get must be implemented in your cache class")
